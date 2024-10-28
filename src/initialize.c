43920da936278e9995150dc34ae369d32b7ce93c
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-10-28 16:02:03 -0400

    reads input file

---
+++ src/initialize.c
@@ -0,0 +1,21 @@
+// LAMMPS 2001 - Molecular Dynamics Simulator
+// Sandia National Laboratories, www.cs.sandia.gov/~sjplimp/lammps.html
+// Steve Plimpton, sjplimp@sandia.gov
+//
+// Copyright (1998-2001) Sandia Corporation.  Under the terms of Contract
+// DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+// certain rights in this software.  This software is distributed under 
+// the GNU General Public License.
+//
+// See the README file in the top-level LAMMPS directory.
+
+// -------------------------------------------------------------------------
+// initialize run parameters
+// -------------------------------------------------------------------------
+
+extern int iversion;
+
+void initialize (void)
+{
+	iversion = 2001;
+}
