76c6d3cdd92809b063d106d8aff133ee61134e69
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-10-28 22:17:13 -0400

    initializes some defaults
    NOTE
    we have not initialized all the params that the original source code
    initializes yet just because we don't intend to do a full rewrite
    or something like that

--- src/initialize.c
+++ src/initialize.c
@@ -15,7 +15,73 @@
 
 extern int iversion;
 
+extern double boltz;
+extern double dtfactor;
+extern double pfactor;
+extern double efactor;
+extern double two_1_3;
+
+extern int units;
+extern int idimension;
+extern int nonperiodic;
+extern double skin;
+
+extern double dt;
+
+extern int volstyle;
+extern int tempstyle;
+extern int pressstyle;
+
+extern int nonstyle;
+extern double cutlj;
+extern int coulstyle;
+
+extern int nfixes;
+
+extern int firstflag;
+extern int readflag;
+
 void initialize (void)
 {
 	iversion = 2001;
+
+	// constants - init default is conventional units
+
+	boltz = 0.001987191;
+	dtfactor = 48.88821;
+	pfactor = 68589.796;
+	efactor = 332.0636;
+	two_1_3 = 1.2599210498948732;
+
+	// values that must be set before read_data or read_restart commands
+
+	units = 0;
+	idimension = 3;
+	nonperiodic = 0;
+	skin = 2.0;
+
+	// integrator settings
+
+	dt = 1.0;
+
+	// ensemble settings
+
+	volstyle = 0;
+	tempstyle = 0;
+	pressstyle = 0;
+
+	// forcefield settings
+
+	nonstyle = 1;
+	cutlj = 10.0;
+	coulstyle = 1;
+
+	// no fixes
+
+	nfixes = 0;
+
+	// no commands have been read-in yet
+
+	firstflag = 0;
+	readflag = 0;
 }
